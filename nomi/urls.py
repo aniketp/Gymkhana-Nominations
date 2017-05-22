44955b951ce5358484499736f7ec432727f76b22
lordaniket06 <aniketp@iitk.ac.in>
2017-05-23 03:46:00 +0530

    Added results view

--- nomi/urls.py
+++ nomi/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # nominations/
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$' , views.nomi_apply, name='nomi_apply'),
+
+    # nominations/2
+    url(r'^(?P<pk>\d+)/$', views.nomi_apply, name='nomi_apply'),
+
+    # nominations/result
+    url(r'^result/$', views.ResultView.as_view(), name='result')
 
 ]
--- nomi/views.py
+++ nomi/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from .models import Nomination,NominationInstance
-from django.http import HttpResponse , HttpResponseRedirect
+from .models import Nomination, NominationInstance
 from django.contrib.auth.decorators import login_required ,permission_required
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
-from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
-
+from django.views import generic
 
 
 def index(request):
@@ -27,5 +23,13 @@ def nomi_apply(request, pk):
         info = "You have applied for it already."
 
     return render(request, 'nomi_done.html', context={'info': info})
+
+
+class ResultView(generic.ListView):
+    template_name = 'result.html'
+    context_object_name = 'all_nominations'
+
+    def get_queryset(self):
+        return NominationInstance.objects.filter(status__exact='a')
 
 
