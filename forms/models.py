103e7d7f77e5f3b6ba06bd9507e609316dfd7b51
aasis21 <ak.ashish2108@gmail.com>
2017-05-25 19:22:10 +0530

    added questions model

--- forms/models.py
+++ forms/models.py
@@ -1,7 +1,7 @@
-
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+from .forms1 import NominationForm
 
 
 class Questionnaire(models.Model):
@@ -15,3 +15,76 @@ class Questionnaire(models.Model):
     def __str__(self):
         return self.name
 
+
+    def get_form(self, *args, **kwargs):
+        fields = []
+        for question in self.question_set.all():
+            field = question._get_formfield_class()
+            label = question.question
+
+            field_args = question._get_field_args()
+
+            ques_id=question.id
+
+            fields.append((label, field, field_args,ques_id))
+
+
+        return NominationForm(*args, extra=fields, **kwargs)
+
+
+
+    def add_answer(self, applicant, answer_data):
+        answerform = FilledForm(questionnaire=self, applicant=applicant)
+        answerform.save()
+        id=answerform.id
+        for ques_id in answer_data:
+            ques=Question.objects.get(pk=ques_id)
+            AnswerInstance.objects.create(form=answerform,question=ques,answer=answer_data[ques_id])
+
+
+
+
+
+FIELD_TYPES = (
+    ('Short_answer', forms.CharField),
+    ('Paragraph', forms.CharField),
+    ('Integer', forms.IntegerField),
+    ('ChoiceField', forms.ChoiceField),
+    ('MultipleChoiceField', forms.MultipleChoiceField),
+    ('Date', forms.DateField),
+)
+
+QUES_TYPES = (
+    ('Short_answer','Short-answer'),
+    ('Paragraph', 'long-answer'),
+    ('Integer', 'Integer-answer'),
+    ('ChoiceField', 'Choice'),
+    ('MultipleChoiceField','Multiple-choice'),
+    ('Date','date'),
+)
+
+class Question(models.Model):
+    questionnaire = models.ForeignKey(Questionnaire, null=True)
+    question_type = models.CharField(max_length=50, choices=QUES_TYPES, null=True)
+    question = models.CharField(max_length=300, null=True)
+    question_choices = models.TextField(max_length=512, null=True, blank=True, help_text='add dollar($) symbol between two choices')
+
+    def __unicode__(self):
+        return self.question
+
+    def __str__(self):
+        return self.question
+
+    def _get_formfield_class(self):
+        for index, field_class in FIELD_TYPES:
+            if self.question_type == index:
+                return field_class
+
+    def _get_field_args(self):
+        args = {}
+        if self.question_type =='ChoiceField' or self.question_type== 'MultipleChoiceField':
+            args['choices'] = enumerate(self.question_choices.split('$'))
+
+
+        args.update({'required': True})
+        return args
