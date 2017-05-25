2e39006a20478bad8bf2e84451978d71aa3954fc
lordaniket06 <aniketp@iitk.ac.in>
2017-05-26 00:27:09 +0530

    Temporary fix for forms view

--- forms/urls.py
+++ forms/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    url(r'^$',views.index, name='index'),
+    url(r'^$', views.index, name='index1'),
     url(r'^create/$', views.build_form, name='build_form'),
     url(r'^(?P<pk>\d+)/$', views.show_form, name='show_form'),
     url(r'^create/ques/(?P<pk>\d+)/$', views.add_ques, name='add_ques'),
