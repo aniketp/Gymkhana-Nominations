5e8e12402ff5fdfb8f08bb3abb08f9fbc12d74a4
aasis21 <ak.ashish2108@gmail.com>
2017-05-25 19:24:12 +0530

    added form_dynamic

---
+++ forms/form_dynamic.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class NominationForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        extra = kwargs.pop('extra')
+        super(NominationForm, self).__init__(*args, **kwargs)
+
+        for i, values in enumerate(extra):
+            label, klass, field_args,ques_id = values
+            self.fields['%s' % ques_id] = klass(label=label, **field_args)
