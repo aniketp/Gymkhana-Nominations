60a5a1d4c63e5a5e019524b0e679cbd64bb0612a
aasis21 <ak.ashish2108@gmail.com>
2017-05-27 18:20:04 +0530

    added views in nomi to show and fill forms

--- nomi/views.py
+++ nomi/views.py
@@ -96,20 +96,31 @@ class UserProfileUpdate(UpdateView):
 
 
 
-def nomination_package(request, pk):
-    questionnaire = get_object_or_404(Questionnaire, id=pk)
+def nomination_create(request):
+
     if request.method == 'POST':
-        pk = questionnaire.pk
         title_form = NominationForm(request.POST)
-        form = questionnaire.get_form(request.POST)
-        if form.is_valid():
-            questionnaire.add_answer(request.user, form.cleaned_data)
+        if title_form.is_valid():
+
+            questionnaire = Questionnaire.objects.create(name=title_form.cleaned_data['title'],
+                                                         description=title_form.cleaned_data['description'])
+            nomination=Nomination.objects.create(name=title_form.cleaned_data['title'],
+                                                 description=title_form.cleaned_data['description'],
+                                                 nomi_form=questionnaire)
+            pk=questionnaire.pk
+            return HttpResponseRedirect(reverse('forms:show_form', kwargs={'pk': pk}))
 
     else:
+        title_form=NominationForm()
 
 
-    return render(request, 'forms/d_forms.html', context={'form': form, 'questionnaire':questionnaire,'pk':pk})
+    return render(request, 'nomi/nomination_form.html', context={'form': title_form})
 
 
+
+class NominationUpdate(UpdateView):
+    model = Nomination
+    fields = ['name', 'description']
+    success_url = reverse_lazy('index')
 
 
