08351fc19988e9daf19d94976e798b3ac362cee0
lordaniket06 <aniketp@iitk.ac.in>
2017-05-27 16:32:40 +0530

    added user create form

--- nomi/models.py
+++ nomi/models.py
@@ -6,8 +6,7 @@ class Nomination(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000, null=True, blank=True)
     results_declared = models.BooleanField(default=False)
-    nomi_form=models.ForeignKey('forms.Questionnaire',null=True,blank=True)
-
+    nomi_form=models.ForeignKey('forms.Questionnaire', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -63,8 +62,8 @@ class UserProfile(models.Model):
         (7, '7'), (8, '8'), (9, '9'), (10, '10'), (11, '11'), (12, '12'),
     )
 
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    name = models.CharField(max_length=40, default=user)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=User)
+    name = models.CharField(max_length=40, blank=True)
     roll_no = models.IntegerField(null=True)
     year = models.CharField(max_length=4, choices=YEAR, default='Y16')
     programme = models.CharField(max_length=7, choices=PROGRAMME, default='B.Tech')
@@ -77,10 +76,10 @@ class UserProfile(models.Model):
 
 
 class Post(models.Model):
-    post_name=models.CharField(max_length=500,null=True)
-    parent=models.ForeignKey('self' ,on_delete=models.CASCADE)
-    persons=models.ManyToManyField(User,blank=True)
-    nomination=models.OneToOneField(Nomination,null=True,blank=True)
+    post_name = models.CharField(max_length=500, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    persons = models.ManyToManyField(User, blank=True)
+    nomination = models.OneToOneField(Nomination, null=True, blank=True)
 
 
 
