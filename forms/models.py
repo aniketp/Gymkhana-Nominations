5cfc1d7d024698878d4abc04d078878efcbae4da
aasis21 <ak.ashish2108@gmail.com>
2017-05-27 16:08:21 +0530

    added field on nomination and fixed bug

--- forms/models.py
+++ forms/models.py
@@ -6,7 +6,7 @@ from .form_dynamic import NominationForm
 
 class Questionnaire(models.Model):
     name = models.CharField(max_length=100, null=True)
-    description = models.TextField(max_length=250, default=u"I'm a description!")
+    description = models.TextField(max_length=1000)
     status = models.CharField(max_length=100, null=True, blank=True)
 
     def __unicode__(self):
--- nomi/models.py
+++ nomi/models.py
@@ -6,6 +6,8 @@ class Nomination(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000, null=True, blank=True)
     results_declared = models.BooleanField(default=False)
+    nomi_form=models.ForeignKey('forms.Questionnaire',null=True,blank=True)
+
 
     def __str__(self):
         return self.name
@@ -77,7 +79,8 @@ class UserProfile(models.Model):
 class Post(models.Model):
     post_name=models.CharField(max_length=500,null=True)
     parent=models.ForeignKey('self' ,on_delete=models.CASCADE)
-    persons=models.ManyToManyField(User,null=True)
+    persons=models.ManyToManyField(User,blank=True)
+    nomination=models.OneToOneField(Nomination,null=True,blank=True)
 
 
 
