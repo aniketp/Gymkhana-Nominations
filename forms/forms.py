73a7cb218ae1dc265dbd393bafa54032523095a1
aasis21 <ak.ashish2108@gmail.com>
2017-05-30 13:25:05 +0530

    added wiget textarea

--- forms/forms.py
+++ forms/forms.py
@@ -11,4 +11,4 @@ class BuildQuestion(forms.Form):
 
     question_type = forms.CharField(max_length=50,widget=forms.Select(choices=QUES_TYPES))
     question = forms.CharField(max_length=300)
-    question_choices = forms.CharField(max_length=512, help_text='add dollar($) symbol between two choices',required=False)
+    question_choices = forms.CharField(max_length=512,widget=forms.Textarea, help_text='add dollar($) symbol between two choices',required=False)
