80f22bafb61763e955dbb2ed2ec6f5d635b21fdc
lordaniket06 <aniketp@iitk.ac.in>
2017-06-04 01:56:35 +0530

    Template modifications

--- nomi/filters.py
+++ nomi/filters.py
@@ -5,6 +5,6 @@ from .models import UserProfile
 class UserProfileFilter(django_filters.FilterSet):
     class Meta:
         model = UserProfile
-        fields = ['user', 'name', 'roll_no', 'year', 'programme', 'department', 'hall']
+        fields = ['name', 'roll_no', 'year', 'programme', 'department', 'hall']
 
 
