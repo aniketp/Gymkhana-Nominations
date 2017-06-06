96209fb61db7280556ff2eb1741a770e032229dc
aasis21 <ak.ashish2108@gmail.com>
2017-06-06 13:54:58 +0530

    added post status in choices

--- nomi/choices.py
+++ nomi/choices.py
@@ -45,6 +45,13 @@ STATUS = (
         ('Compiled', 'Compiled'),
 )
 
+POST_STATUS = (
+        ('Post created', 'Post created'),
+        ('Post approved', 'Post approved'),
+        ('Post rejected', 'Post rejected'),
+        ('Post on work', 'Post on work'),
+)
+
 HALL_1 = (
     ('All', 'All'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'),
     ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12'),
