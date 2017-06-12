8a811cfafd4a89291c1c5c06387192ee2db5ebc8
aasis21 <ak.ashish2108@gmail.com>
2017-06-13 00:46:05 +0530

    added working filter to main index template

--- nomi/global_contexts.py
+++ nomi/global_contexts.py
@@ -1,9 +1,9 @@
-from .models import POST,UserProfile
+from .models import Post,UserProfile
 
 def context(request):
     if request.user.is_authenticated:
-        my_profile = UserProfile.objects.get(request.user)
+        my_profile = UserProfile.objects.get(user=request.user)
         my_posts = Post.objects.filter(post_holders=request.user)
         return {'my_posts':my_posts,'my_profile':my_profile}
     else:
-        return None
+        return {'my_posts': 0, 'my_profile': 0}
