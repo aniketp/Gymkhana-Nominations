f0c57d7b0d3848e21157a0f1caec2dd3004941cb
aasis21 <ak.ashish2108@gmail.com>
2017-06-13 00:10:02 +0530

    added context function to global context to pass post and profile info in all views

--- nomi/global_contexts.py
+++ nomi/global_contexts.py
@@ -2,6 +2,8 @@ from .models import POST,UserProfile
 
 def context(request):
     if request.user.is_authenticated:
-
-
-    return {'notice':notice,'note':note}
+        my_profile = UserProfile.objects.get(request.user)
+        my_posts = Post.objects.filter(post_holders=request.user)
+        return {'my_posts':my_posts,'my_profile':my_profile}
+    else:
+        return None
