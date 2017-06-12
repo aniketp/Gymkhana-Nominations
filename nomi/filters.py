33b727e502ee0d5a41700d2bf512ddb450bbcb28
aasis21 <ak.ashish2108@gmail.com>
2017-06-12 17:19:47 +0530

    created filter on nomination based on clubs

---
+++ nomi/filters.py
@@ -0,0 +1,7 @@
+import django_filters
+from .models import Nomination
+
+class NominationFilter(django_filters.FilterSet):
+    class Meta:
+        model = Nomination
+        fields = ['nomi_approvals__club', 'year_choice']
