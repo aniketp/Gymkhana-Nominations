e510d6386fd119e8036b1198f6a66fd7b697e86d
lordaniket06 <aniketp@iitk.ac.in>
2017-06-15 23:14:47 +0530

    Added dummy data

--- forms/forms.py
+++ forms/forms.py
@@ -7,7 +7,8 @@ class BuildQuestion(forms.Form):
 
     question_type = forms.CharField(max_length=50,widget=forms.Select(choices=QUES_TYPES))
     question = forms.CharField(max_length=300)
-    question_choices = forms.CharField(max_length=512,widget=forms.Textarea, help_text='add dollar($) symbol between two choices',required=False)
+    question_choices = forms.CharField(max_length=512,widget=forms.Textarea,
+                                       help_text='add choices in separate lines', required=False)
 
 # no use as of now
 class BuildForm(forms.Form):
--- nomi/models.py
+++ nomi/models.py
@@ -26,6 +26,7 @@ class Post(models.Model):
     post_holders = models.ManyToManyField(User, blank=True)
     post_approvals = models.ManyToManyField('self', related_name='approvals', symmetrical=False, blank=True)
     status = models.CharField(max_length=50, choices=POST_STATUS, default='Post created')
+    status = models.CharField(max_length=50, choices=POST_STATUS, default='Post created')
     perms = models.CharField(max_length=200, choices=POST_PERMS, default='normal')
 
 
