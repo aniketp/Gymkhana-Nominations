229a9bad51d6605ecec974aa33bb104c5b0c5182
lordaniket06 <aniketp@iitk.ac.in>
2017-06-17 23:11:21 +0530

    refactored views functions

--- forms/views.py
+++ forms/views.py
@@ -1,35 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse,reverse_lazy
-from .models import Questionnaire, Question,FilledForm,QUES_TYPES
+from django.core.urlresolvers import reverse
+from .models import Questionnaire, Question, FilledForm
 from .forms import BuildForm, BuildQuestion
+from django.views.generic.edit import UpdateView
 import json
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # not in use
 def index(request):
     forms = Questionnaire.objects.all()
     return render(request, 'forms/index.html', context={'forms': forms})
 
-#this is used to create and edit nomination forms..
+
 def creator_form(request, pk):
     questionnaire = get_object_or_404(Questionnaire, id=pk)
     form = questionnaire.get_form(request.POST or None)
     pk = questionnaire.pk
-    questions=Question.objects.filter(questionnaire=questionnaire)
+    questions = Question.objects.filter(questionnaire=questionnaire)
 
-    return render(request, 'forms/creator_form1.html', context={'form': form,'questions':questions, 'questionnaire':questionnaire,'pk':pk})
+    return render(request, 'forms/creator_form.html', context={'form': form, 'questions': questions,
+                                                               'questionnaire': questionnaire, 'pk': pk})
 
 
-# used to add ques in forms
 def add_ques(request, pk):
     questionnaire = Questionnaire.objects.get(pk=pk)
 
     if request.method == 'POST':
         form = BuildQuestion(request.POST)
         if form.is_valid():
-            Question.objects.create(questionnaire=questionnaire, question_type=form.cleaned_data['question_type'], question=form.cleaned_data['question'], question_choices=form.cleaned_data['question_choices'])
+            Question.objects.create(questionnaire=questionnaire, question_type=form.cleaned_data['question_type'],
+                                    question=form.cleaned_data['question'],
+                                    question_choices=form.cleaned_data['question_choices'])
 
             return HttpResponseRedirect(reverse('forms:creator_form', kwargs={'pk': pk}))
     else:
@@ -37,11 +40,11 @@ def add_ques(request, pk):
 
     return render(request, 'forms/build_ques.html', context={'form': form, 'questionnaire': questionnaire})
 
-#used to edit questions
+
 class QuestionUpdate(UpdateView):
     model = Question
-    fields = ['question','question_type','question_choices']
-    template_name='forms/ques_update.html'
+    fields = ['question', 'question_type', 'question_choices']
+    template_name = 'forms/ques_update.html'
 
     def get_success_url(self):
 
@@ -49,9 +52,6 @@ class QuestionUpdate(UpdateView):
         return reverse('forms:creator_form', kwargs={'pk': qk})
 
 
-
-#----------------------------------------------------------------------------
-# no use as of now
 def show_form(request, pk):
     questionnaire = get_object_or_404(Questionnaire, id=pk)
     form = questionnaire.get_form(request.POST or None)
@@ -65,7 +65,6 @@ def show_form(request, pk):
     return render(request, 'forms/show_form.html', context={'form': form, 'questionnaire': questionnaire, 'pk': pk})
 
 
-# no use as of now
 def show_answer_form(request, pk):
     questionnaire = get_object_or_404(Questionnaire, id=pk)
     filled_form = FilledForm.objects.filter(questionnaire=questionnaire).filter(applicant=request.user)
@@ -75,7 +74,6 @@ def show_answer_form(request, pk):
     return render(request, 'forms/ans_form.html', context={'form': form})
 
 
-# no use as of now..
 def build_form(request):
     if request.method == 'POST':
         form = BuildForm(request.POST)
