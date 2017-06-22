cab5ff9ca9ac76e2e6a3379080dbc2d14ef17641
lordaniket06 <aniketp@iitk.ac.in>
2017-06-23 02:17:51 +0530

    Indentation in choices

--- nomi/choices.py
+++ nomi/choices.py
@@ -4,33 +4,33 @@ PROGRAMME = (
 )
 
 DEPT = (
-    ('Aerospace Engineering', 'AE'),
-    ('Biological Sciences & Engineering', 'BSBE'),
-    ('Chemical Engineering', 'CHE'),
-    ('Civil Engineering', 'CE'),
-    ('Computer Science & Engineering', 'CSE'),
-    ('Electrical Engineering', 'EE'),
-    ('Materials Science & Engineering', 'MSE'),
-    ('Mechanical Engineering', 'ME'),
-    ('Industrial & Management Engineering', 'IME'),
-    ('Chemistry', 'CHM'),
-    ('Mathematics & Scientific Computing', 'MTH'),
-    ('Physics', 'PHY'),
-    ('Earth Sciences', 'ES')
+        ('Aerospace Engineering', 'AE'),
+        ('Biological Sciences & Engineering', 'BSBE'),
+        ('Chemical Engineering', 'CHE'),
+        ('Civil Engineering', 'CE'),
+        ('Computer Science & Engineering', 'CSE'),
+        ('Electrical Engineering', 'EE'),
+        ('Materials Science & Engineering', 'MSE'),
+        ('Mechanical Engineering', 'ME'),
+        ('Industrial & Management Engineering', 'IME'),
+        ('Chemistry', 'CHM'),
+        ('Mathematics & Scientific Computing', 'MTH'),
+        ('Physics', 'PHY'),
+        ('Earth Sciences', 'ES')
 )
 
 YEAR = (
-    ('Y16', 'Y16'),
-    ('Y15', 'Y15'),
-    ('Y14', 'Y14'),
-    ('Y13', 'Y13'),
-    ('Y12', 'Y12'),
-    ('Y11', 'Y11'),
+        ('Y16', 'Y16'),
+        ('Y15', 'Y15'),
+        ('Y14', 'Y14'),
+        ('Y13', 'Y13'),
+        ('Y12', 'Y12'),
+        ('Y11', 'Y11'),
 )
 
 HALL = (
-    ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'),
-    ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12'),
+        ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'),
+        ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12'),
 )
 
 NOMI_STATUS = (
@@ -39,8 +39,8 @@ NOMI_STATUS = (
 )
 
 INTERVIEW_STATUS = (
-    ('Interview Not Done', 'Interview Not Done'),
-    ('Interview Done', 'Interview Done'),
+        ('Interview Not Done', 'Interview Not Done'),
+        ('Interview Done', 'Interview Done'),
 )
 
 STATUS = (
@@ -59,14 +59,14 @@ POST_STATUS = (
 )
 
 POST_PERMS = (
-    ("normal", "normal"),
-    ("can ratify the post", "can ratify the post"),
-    ("can approve post and send nominations to users", "can approve post and send nominations to users"),
+        ("normal", "normal"),
+        ("can ratify the post", "can ratify the post"),
+        ("can approve post and send nominations to users", "can approve post and send nominations to users"),
 )
 
 HALL_1 = (
-    ('All', 'All'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'),
-    ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12'),
+        ('All', 'All'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'),
+        ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12'),
 )
 
 DEPT_1 = (
