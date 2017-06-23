a7b915dc875269d46f5841f160d7b9aa93186d65
lordaniket06 <aniketp@iitk.ac.in>
2017-06-23 21:54:37 +0530

    Created club tag model

--- info/models.py
+++ info/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ClubTag(models.Model):
+    tag_name = models.CharField(max_length=50, null=True, default=True)
+
+    def __str__(self):
+        return self.tag_name
+
+
