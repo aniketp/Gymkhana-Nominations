88b2ca431747528bd9fb4447ddd85501727c9044
aasis21 <ak.ashish2108@gmail.com>
2017-06-24 20:27:20 +0530

    added media in urls.py and image url in template

--- gymkhana/urls.py
+++ gymkhana/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
-
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -11,3 +12,6 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='nominations')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
