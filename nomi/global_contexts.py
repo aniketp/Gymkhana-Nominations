71d102f86f1042f049d2eda97aa49d13965f2005
lordaniket06 <aniketp@iitk.ac.in>
2017-06-25 04:41:53 +0530

    Passed senate in global context

--- nomi/global_contexts.py
+++ nomi/global_contexts.py
@@ -7,6 +7,14 @@ def context(request):
         try:
             my_profile = UserProfile.objects.get(user=request.user)
             my_posts = Post.objects.filter(post_holders=request.user)
+
+            for post in my_posts:
+                senate = None
+                if post.perms == 'can ratify the post':
+                    senate = True
+
+                return {'my_posts': my_posts, 'my_profile': my_profile, 'senate': senate}
+
             return {'my_posts': my_posts, 'my_profile': my_profile}
         except ObjectDoesNotExist:
             return {'my_posts': 0, 'my_profile': 0}
