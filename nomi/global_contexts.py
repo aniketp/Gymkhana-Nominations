97817a588f9f50c313b24e61c2d688c9b4028002
lordaniket06 <aniketp@iitk.ac.in>
2017-06-25 05:31:44 +0530

    Correction in global_context.py

--- nomi/global_contexts.py
+++ nomi/global_contexts.py
@@ -8,15 +8,14 @@ def context(request):
             my_profile = UserProfile.objects.get(user=request.user)
             my_posts = Post.objects.filter(post_holders=request.user)
             length = len(my_posts)
+            senate = False
 
             for post in my_posts:
-                senate = None
                 if post.perms == 'can ratify the post':
                     senate = True
 
-                return {'my_posts': my_posts, 'my_profile': my_profile, 'senate': senate, 'length': length}
+            return {'my_posts': my_posts, 'my_profile': my_profile, 'senate': senate, 'length': length}
 
-            return {'my_posts': my_posts, 'my_profile': my_profile, 'length': length}
         except ObjectDoesNotExist:
             return {'my_posts': 0, 'my_profile': 0}
     else:
