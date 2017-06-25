739a736b89013be37438f5f5b58f2366a564967a
aasis21 <ak.ashish2108@gmail.com>
2017-06-25 19:21:57 +0530

    implemented filter in info view

--- info/views.py
+++ info/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from nomi.models import Post,Club
 from info.filters import PostFilter
+from info.forms import ClubForm,PostForm
 
 
 def index(request):
-    user_list = PostFilter(request.GET, queryset=Post.objects.all().order_by('post_name').distinct())
+    club = None
 
-    return render(request, 'index.html', context={'filter': user_list})
+    if request.method == 'POST':
+        club_form = ClubForm(request.POST)
+        post_form = PostForm(club,request.POST)
+
+
+        if club_form.is_valid():
+            club = Club.objects.get(pk = club_form.cleaned_data['club'])
+
+            if post_form.is_valid():
+                post = Post.objects.filter(pk=post_form.cleaned_data['post'],tag = club)
+                query_posts = post
+                post_form = PostForm(club)
+                return render(request, 'index.html',
+                              context={'club_form': club_form, 'post_form': post_form,
+                                       'query_posts': query_posts})
+
+            post_form = PostForm(club)
+            query_posts = club.club_posts.all()
+            return render(request, 'index.html',
+                          context={'club_form': club_form,'post_form':post_form ,
+                                   'query_posts': query_posts})
+
+
+    else:
+        club_form = ClubForm
+        post_form = None
+        query_posts = Post.objects.all()
+
+    return render(request, 'index.html', context={'club_form':club_form,'post_form':post_form,
+                                                  'query_posts':query_posts})
